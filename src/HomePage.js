a514d97dfaa860f0a68c98082041c8f2efb8387a
rezmayyy <austinmann12@gmail.com>
2023-10-02 10:05:42 -0700

    Homepage: added container for devices w 2 columns.

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,26 +7,13 @@ export const { client } = vendiaClient();
 
 export const HomePage = () => {
 
-  // const [device, setDevice] = useState('')
-  // const [testID, setTestID] = useState(0)
-  // const [testList, setTestList] = useState()
-
-  // const {device, testID, testList} = useContext(DataContext);
-  // const [stateDevice, setStateDevice] = device;
-  // const [stateTestID, setStateTestID] = testID;
-  // const [stateTestList, setStateTestList] = testList;
   const testList = useContext(DataContext).testList[0]
-
-  const testingStuff = () => {
-    console.log(testList)
-  }
 
   return (
     <div>
-      <h1>Algorithm Allies Team 6</h1>
-      <button onClick={testingStuff}>test</button>
-      <div>
-        <h2>Device List:</h2>
+      <h1>Algorithm Allies Team 6</h1> 
+      <h2>Device List:</h2>   
+      <div className="container">
         {testList?.map((item, index) => (
           <div key={index} className="item-box">
             {item.Device}
