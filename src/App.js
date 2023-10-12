2fe963751206d213ce0561d9849509b3741dace4
tonymai9 <tonymainine@gmail.com>
2023-10-12 13:14:49 -0700

    able to change page manually through url

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './styles/App.css';
 import { HomePage } from './pages/HomePage';
 import { FormPage } from './pages/FormPage';
 import { DataProvider } from './context/dataContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <DataProvider>
-          {/*<HomePage />*/}
-          <FormPage />
+          <BrowserRouter>
+            <Routes>
+              <Route>
+                <Route index element={<HomePage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/form" element={<FormPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
         </DataProvider>
       </header>
     </div>
