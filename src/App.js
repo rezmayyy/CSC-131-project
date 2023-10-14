beccfc6234974824a3ba2cb06718a10a1d4c0f09
Brycee916 <brycechao27@gmail.com>
2023-10-14 14:18:11 -0700

    firebase fix 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './styles/App.css';
 import { HomePage } from './pages/HomePage';
+import { TestlistPage }  from './pages/TestlistPage';
+import { LoginPage } from './pages/LoginPage';
 import { FormPage } from './pages/FormPage';
 import { DataProvider } from './context/dataContext';
 import { Routes, Route } from 'react-router-dom'
@@ -9,9 +11,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <LoginPage />
         <DataProvider>
+          
           <Routes>
             <Route>
+              
               <Route path="/" element={<HomePage />} />
               <Route path="/form" element={<FormPage />} />
               <Route path="/testlist/:deviceName" element={<TestlistPage />} />
