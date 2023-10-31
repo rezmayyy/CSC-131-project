87856ec009a7fa3bd1c7202a5b1ca2842d1f312c
rezmayyy <austinmann12@gmail.com>
2023-10-30 19:37:38 -0700

    login error message from firebase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -12,26 +12,30 @@ export const LoginPage = () => {
     const [password, setPassword] = useContext(AuthContext).password;
     const [user, setUser] = useContext(AuthContext).user;
     const navigate = useNavigate();
+    const [error, setError] = useState(null);      
 
     const signIn = async () => {
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user
+            const user = userCredential.user;
             localStorage.setItem('token', user.accessToken);
             localStorage.setItem('user', JSON.stringify(user));
             setEmail("");
             setPassword("");
+            setError(null); 
         } catch (err) {
-            console.error(err);
+            setError(err.message); 
         }
     };
 
-    const logoutButton = useContext(AuthContext).logoutButton
+    const logoutButton = useContext(AuthContext).logoutButton;
     
     const handleSubmit = async (e) => {
         e.preventDefault();
         await signIn();
-        navigate("/");
+        if (user) {
+            navigate("/");
+        }
     }
 
     return (
@@ -39,25 +43,21 @@ export const LoginPage = () => {
             <h2 id="subtitle-name">Login Page</h2>
             <div className="signup-page-content">
                 <form onSubmit={handleSubmit}>
-
                     <input
                         placeholder="Email..."
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
-
-
                     <input
                         placeholder="Password..."
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-
-                    { !user && <Button type="submit" variant="secondary" className="signup-button"> Sign In</Button>}
-                    { user && <Button type="button" variant="secondary" className="signup-button" onClick={logoutButton}> Logout </Button>}
+                    {error && <p className="error-message">{error}</p>}
+                    {!user && <Button type="submit" variant="secondary" className="signup-button">Sign In</Button>}
+                    {user && <Button type="button" variant="secondary" className="signup-button" onClick={logoutButton}>Logout</Button>}
                 </form>
-
                 <div className="general-div">
                     {user ? (
                         <p>You are logged in as {user.email}</p>
