08ddbce1aa867b33bbba0ac4a92f060e7a9fc0ba
tonymai9 <tonymainine@gmail.com>
2023-10-30 22:41:50 -0700

    delete on HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -42,9 +42,37 @@ export const HomePage = () => {
     setDeviceList(listDeviceResponse?.items);
   }
 
+  const handleDelete = (event) => {
+    // setDevice(event.target.id.Device)
+    // deleteAllTest(event.target.id.Device)
+    // deleteDevice(event.target.id._id)
+    console.log(event.target.id)
+
+  }
+
+  const deleteDevice = async (value) => {
+    const deleteDevice = await client.entities.device.remove(value)
+    console.log(deleteDevice)
+  }
+
+  const deleteAllTest = async (value) => {
+    const checkResponseTest = await client.entities.test.list({
+      filter: {
+        Device: {
+          eq: value,
+        },
+      },
+    })
+
+    for(let i = 0; i < checkResponseTest.items.length; i++){
+      const deleteTest = await client.entities.test.remove(checkResponseTest.items[i]._id)
+      console.log(deleteTest)
+    }
+    
+  }
   return (
     <div>
-      <div><h1 className="title-header"><img src="AlgorithmAlliesLogo.png"/>Algorithm Allies Team 6</h1></div>
+      <div><h1 className="title-header"><img src="AlgorithmAlliesLogo.png" />Algorithm Allies Team 6</h1></div>
       <div><h2 id="subtitle-name">Device List:</h2></div>
       <div id="search-for-device">
         <input id="search-for-device-input"
@@ -68,11 +96,12 @@ export const HomePage = () => {
             <Link to={`/testlist/${item.Device}`} className="custom-link">
               <Button className="button-shadow-effects" variant="secondary">View Test</Button>
             </Link>
+            <Button className="delete-device-button" variant="secondary" id={item} onClick={handleDelete}>Delete</Button>
           </div>
         )
         )}
         <div className="item-box">
-          <DeviceNameInput id="add-device-input"/>
+          <DeviceNameInput id="add-device-input" />
           <Button id="add-device-button" variant="primary" onClick={addDevice}>+</Button>
         </div>
       </div>
